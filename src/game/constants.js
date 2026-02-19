ecd81967efc49908500b7654e4f18dd78642bb9a
Noah Zixin Zhang <157932112+Learnmore-smart@users.noreply.github.com>
2026-02-19 18:50:54 -0500

    Change MAX_BATTLE_DURATION_TICKS to 12000
    Updated MAX_BATTLE_DURATION_TICKS to reflect new duration.
    
    10 minutes

--- src/game/constants.js
+++ src/game/constants.js
@@ -33,7 +33,7 @@ module.exports = {
   MAX_ACTIONS_PER_SECOND: 30,
 
   // Battle
-  MAX_BATTLE_DURATION_TICKS: 100000, // 2 minutes at 20 TPS
+  MAX_BATTLE_DURATION_TICKS: 12000, // 2 minutes at 20 TPS
 
   // Directions map
   DIRECTIONS: {
